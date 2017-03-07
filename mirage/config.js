c3d312d921508174bd6f37e81a51d9077988eee0
P. Douglas Reeder <reeder.29@gmail.com>
2017-03-07 00:41:40 -0500

    Uses ember-data, so mock data is available to all routes

---
+++ mirage/config.js
@@ -0,0 +1,56 @@
+export default function() {
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
+  */
+
+  var lipsum = "lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat".split(" ");
+
+  var data = [];
+  for (var i = 0; i < 500; i++) {
+    var text = [];
+    for (var j=0; j<Math.random()*60; j++) {
+      text.push (lipsum[Math.floor(Math.random()*lipsum.length)]);
+    }
+    text.push(i);
+    let priority;
+    if (i<=1) {
+      priority = "adopted";
+    } else if (i >=20) {
+      priority = "dismissed";
+    } else {
+      priority = "";
+    }
+    let item = {name: text.join(" "), completed: !Boolean((i+1)%3), priority: priority };
+    data.push({type: 'item', id: i, attributes: item});
+  }
+
+
+  this.namespace = '/api';
+
+  this.get('/items', function () {
+    return {
+      data: data
+    };
+  });
+}
