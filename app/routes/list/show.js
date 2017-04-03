7fc8439d7a0e6fcf745ec0ae2837f68a312d2868
P. Douglas Reeder <reeder.29@gmail.com>
2017-04-03 00:36:05 -0400

    Tapping on list item scrolls to detail panel, in 1-panel mode

--- app/routes/list/show.js
+++ app/routes/list/show.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('item', params.item_id);
+  },
+
+  actions: {
+    didTransition() {
+      console.log("didTransition");
+      // $('.panelContainer')[0].scrollLeft = 10000;
+      $('.panelContainer').animate({
+        scrollLeft: $('.panelContainer').width()
+      }, {
+        duration: 330
+      });
+      return true; // Bubble the didTransition event
+    }
   }
 });
